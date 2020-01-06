f8d2c5e9dc0ae81b29f72a59048dc6d8753f179e
Yair fernando <yair.facio11@gmail.com>
2020-01-06 11:13:13 -0600

    Setting up the basic of the table component

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
 import Jumbotron from './Jumbotron'
+import Table from './Table/Table';
 
 class Home extends Component {
   constructor(props) {
@@ -20,6 +21,7 @@ class Home extends Component {
     return(
       <div>
         <Jumbotron />
+        <Table />
       </div>
     )
   };
