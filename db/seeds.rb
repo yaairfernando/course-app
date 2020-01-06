f7f00006fd592dd0431fa250663f7553d81667f4
Yair fernando <yair.facio11@gmail.com>
2020-01-06 14:42:07 -0600

    Adding episodes method to our pages controller in rails app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+course = Course.create(title: "Hello World", description: "Create a react app with ruby and rails")
+
+section = Section.create(title: "Chapter 1", course:course)
+
+episode = Episode.create([
+  { 
+    title: 'Setting up your rails application', 
+    description: 'Create your rails application with easy steps', 
+    url: 'https://www.youtube.com/embed/DLX62G4lc44', 
+    section: section
+  },{ 
+    title: 'Implementing react inside your rails application', 
+    description: 'Inject react to your application', 
+    url: 'https://www.youtube.com/embed/DLX62G4lc44', 
+    section: section
+  },{ 
+    title: 'Building a Hello World', 
+    description: 'Build greate application', 
+    url: 'https://www.youtube.com/embed/DLX62G4lc44', 
+    section: section
+  },{ 
+    title: 'Adding React Router DOM to your application', 
+    description: 'Adding new libraries to your rails application from react', 
+    url: 'https://www.youtube.com/embed/DLX62G4lc44', 
+    section: section
+  }
+])
