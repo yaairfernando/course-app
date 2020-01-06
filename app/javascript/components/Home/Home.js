81e74e0db814ada222f2332ae4b7f8ff6798d2ad
Yair fernando <yair.facio11@gmail.com>
2020-01-06 11:09:18 -0600

    Finished jumbotron component

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -1,23 +1,26 @@
 import React, { Component} from 'react';
+import Jumbotron from './Jumbotron'
 
 class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      modules: [
+      course_modules: [
         { id: 1, title: 'Setting up your rails application', description: 'Create your rails application with easy steps', active: false},
         { id: 2, title: 'Implementing react inside your rails application', description: 'Inject react to your application', active: false},
         { id: 3, title: 'Building a Hello World', description: 'Build greate application', active: false},
         { id: 4, title: 'Adding React Router DOM to your application', description: 'Adding new libraries to your rails application from react', active: false}
-      ],
+      ]
 
     };
   }
 
   render() {
     return(
-      <div>Home</div>
+      <div>
+        <Jumbotron />
+      </div>
     )
   };
 };
